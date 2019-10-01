0cce02799f68c143f62ed375cbf2205ba0a5beff
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2019-10-01 17:01:40 +0100

    fullrecon: a little bit more clarity with types in the main routine

--- src/main/scala/tapl/fullrecon/demo.scala
+++ src/main/scala/tapl/fullrecon/demo.scala
@@ -16,20 +16,25 @@ object FullReconDemo extends util.Demo[(Context, UVarGenerator, IdConstr), Comma
     FullReconParsers.input(s)(Context())._1
 
   def processCommand(in: (Context, UVarGenerator, IdConstr), cmd: Command): (Context, UVarGenerator, IdConstr) = in match {
-    case (ctx, nextuvar, constr) => cmd match {
+    case (ctx, nextuvar, constr: IdConstr) => cmd match {
       case Eval(t1) =>
 
-        val (tyT, nextuvar1, constrT) = recon(ctx, nextuvar, t1)
-        val constr11 = constr ++ constrT
-        val constr12 = unify(ctx, "Could not simplify constraints", constr11)
+        val (tyT, nextuvar1, constrT: Constr) = recon(ctx, nextuvar, t1)
+        val constr11: Constr =
+          constr ++ constrT
+        val constr12: IdConstr =
+          unify(ctx, "Could not simplify constraints", constr11)
         
         val ty1 = applySub(constr12, tyT)
         val doc1 = g2(ptmATerm(true, ctx, t1) :: ":" :/: ptyTy(ctx, ty1) :: ";")
 
         val t2 = eval(ctx, t1)
-        val (tyT2, nextuvar2, constrT2) = recon(ctx, nextuvar, t1)
-        val constr21 = constr ++ constrT2
-        val constr22 = unify(ctx, "Could not simplify constraints", constr21)
+        val (tyT2, nextuvar2, constrT2: Constr) =
+          recon(ctx, nextuvar, t1)
+        val constr21: Constr =
+          constr ++ constrT2
+        val constr22: IdConstr =
+          unify(ctx, "Could not simplify constraints", constr21)
         val ty2 = applySub(constr22, tyT2)
         val doc2 = g2(ptmATerm(true, ctx, t2) :: ":" :/: ptyTy(ctx, ty1) :: ";")
 
