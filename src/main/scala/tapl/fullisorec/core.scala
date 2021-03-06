f8d2bee2d27a0f9cb60339dbc4b4cec4fc092278
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2021-03-06 09:46:37 +0000

    fullisorec - enums

--- src/main/scala/tapl/fullisorec/core.scala
+++ src/main/scala/tapl/fullisorec/core.scala
@@ -1,6 +1,7 @@
 package tapl.fullisorec
 
 object Util {
+  import Term._
 
   def isNumericVal(ctx: Context, t: Term): Boolean =
     t match {
@@ -27,6 +28,8 @@ object Util {
 object Evaluator {
   import Util._
   import Syntax._
+  import Binding._
+  import Term._
 
   private def eval1(ctx: Context, t: Term): Term =
     t match {
@@ -135,6 +138,9 @@ object Evaluator {
 
 object Typer {
   import Syntax._
+  import Binding._
+  import Term._
+  import Ty._
 
   private def isTyAbb(ctx: Context, i: Int) =
     ctx.getBinding(i) match {
