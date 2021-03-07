a0db125e5e3beb08f6227765b0181498c593e150
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2021-03-07 20:29:27 +0000

    GrammarSpec: restructuring

--- src/test/scala/tapl/test/grammars/GrammarSpec.scala
+++ src/test/scala/tapl/test/grammars/GrammarSpec.scala
@@ -1,52 +1,57 @@
 package tapl.test.grammars
 
+import java.nio.file.{Files, Paths}
+
 import org.antlr.v4.runtime._
 import org.antlr.v4.runtime.tree.ParseTreeWalker
 
-import java.nio.file.Paths
-import tapl.arith.{ArithAstListener, ArithDemo, ArithParsers}
-import tapl.bot.BotDemo
 import tapl.grammars._
 
-import scala.io.Source
-
 class GrammarSpec extends org.scalatest.funspec.AnyFunSpec {
+  import GrammarSpec._
+
   describe("Antlr grammars should recognize examples") {
     it("Arith.g4") {
-      val stream = CharStreams.fromPath(Paths.get(ArithDemo.defaultExample))
-      val lexer = new ArithLexer(stream)
-      val tokens = new CommonTokenStream(lexer)
-      val parser = new ArithParser(tokens)
-      parser.addErrorListener(new FailErrorListener)
-      val tree = parser.program()
-      val walker = new ParseTreeWalker
-      val listener = new ArithAstListener
-      walker.walk(listener, tree)
-      val antlrProgram = listener.program
-      val scalaProgram = ArithParsers.input(Source.fromFile(ArithDemo.defaultExample).mkString(""))
+      import tapl.arith._
 
+      val input = getInput(ArithDemo.defaultExample)
+      val parser =
+        new ArithParser(new CommonTokenStream(new ArithLexer(CharStreams.fromString(input))))
+      parser.addErrorListener(new FailErrorListener)
+      val programContext = parser.program()
+
+      val listener = new ArithAstListener
+      (new ParseTreeWalker).walk(listener, programContext)
+      val antlrProgram = listener.program
+
+      val scalaProgram = ArithParsers.input(input)
       assert(antlrProgram === scalaProgram)
     }
 
     it("Bot.g4") {
-      val stream = CharStreams.fromPath(Paths.get(BotDemo.defaultExample))
-      val lexer = new BotLexer(stream)
-      val tokens = new CommonTokenStream(lexer)
-      val parser = new BotParser(tokens)
+      import tapl.bot._
+
+      val input = getInput(BotDemo.defaultExample)
+      val parser = new BotParser(new CommonTokenStream(new BotLexer(CharStreams.fromString(input))))
       parser.addErrorListener(new FailErrorListener)
       parser.program()
     }
   }
 }
 
-class FailErrorListener extends BaseErrorListener {
-  override def syntaxError(
-      recognizer: Recognizer[_, _],
-      offendingSymbol: Any,
-      line: Int,
-      charPositionInLine: Int,
-      msg: String,
-      e: RecognitionException,
-  ): Unit =
-    sys.error(s"line $line, column $charPositionInLine, $msg")
+object GrammarSpec {
+  def getInput(path: String): String =
+    new String(Files.readAllBytes(Paths.get(path)))
+
+  class FailErrorListener extends BaseErrorListener {
+    override def syntaxError(
+        recognizer: Recognizer[_, _],
+        offendingSymbol: Any,
+        line: Int,
+        charPositionInLine: Int,
+        msg: String,
+        e: RecognitionException,
+    ): Unit =
+      sys.error(s"line $line, column $charPositionInLine, $msg")
+  }
 }
