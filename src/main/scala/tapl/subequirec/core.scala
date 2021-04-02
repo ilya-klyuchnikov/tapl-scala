b71f3d3cf06e3f0e57f8910e2b787178f10df286
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2021-04-02 23:42:05 +0100

    aligned core

--- src/main/scala/tapl/subequirec/core.scala
+++ src/main/scala/tapl/subequirec/core.scala
@@ -191,6 +191,146 @@ object Typer {
     }
   }
 
+  def typeof(ctx: Context, t: Term): Ty =
+    t match {
+      case TmString(_) =>
+        TyString
+      case TmVar(i, _) =>
+        ctx.getType(i)
+      case TmAscribe(t1, tyT) =>
+        if (subtype(ctx, typeof(ctx, t1), tyT))
+          tyT
+        else
+          throw new Exception("body of as-term doesn't have the expected type in " + t)
+      case TmRecord(fields) =>
+        val fieldTys = fields.map { case (li, ti) => (li, typeof(ctx, ti)) }
+        TyRecord(fieldTys)
+      case TmProj(t1, l) =>
+        simplifyTy(ctx, typeof(ctx, t1)) match {
+          case TyRecord(fieldTys) =>
+            fieldTys find { _._1 == l } match {
+              case Some((_, tyi)) => tyi
+              case None           => throw new Exception("Label " + l + " not found in " + t)
+            }
+          case _ => throw new Exception("Expected record type for " + t1)
+        }
+      case TmAbs(v, tyT1, t2) =>
+        val ctx1 = ctx.addBinding(v, VarBind(tyT1))
+        val tyT2 = typeof(ctx1, t2)
+        TyArr(tyT1, typeShift(-1, tyT2))
+      case TmApp(t1, t2) =>
+        val tyT1 = typeof(ctx, t1)
+        val tyT2 = typeof(ctx, t2)
+        simplifyTy(ctx, tyT1) match {
+          case TyArr(tyT11, tyT12) =>
+            if (subtype(ctx, tyT2, tyT11))
+              tyT12
+            else {
+              println(tyT1)
+              println(tyT2)
+              println(simplifyTy(ctx, tyT1))
+              throw new Exception("parameter mismatch in " + t + " : " + tyT2 + " != " + tyT11)
+            }
+          case TyBot => TyBot
+          case z => {
+            println(tyT1)
+            println(tyT2)
+            println(simplifyTy(ctx, tyT1))
+            throw new Exception("arrow type expected in " + t1)
+          }
+        }
+      case TmTrue =>
+        TyBool
+      case TmFalse =>
+        TyBool
+      case TmIf(t1, t2, t3) =>
+        if (subtype(ctx, typeof(ctx, t1), TyBool)) {
+          join(ctx, typeof(ctx, t2), typeof(ctx, t3))
+        } else {
+          throw new Exception("guard of conditional " + t + " is not a boolean")
+        }
+      case TmZero =>
+        TyNat
+      case TmSucc(t1) =>
+        if (subtype(ctx, typeof(ctx, t1), TyNat)) {
+          TyNat
+        } else {
+          throw new Exception("argument of Succ: is not a number: " + t)
+        }
+      case TmPred(t1) =>
+        if (subtype(ctx, typeof(ctx, t1), TyNat)) {
+          TyNat
+        } else {
+          throw new Exception("argument of Pred: is not a number: " + t)
+        }
+      case TmIsZero(t1) =>
+        if (subtype(ctx, typeof(ctx, t1), TyNat)) {
+          TyBool
+        } else {
+          throw new Exception("argument of IsZero: is not a number: " + t)
+        }
+      case TmCase(t, cases) =>
+        simplifyTy(ctx, typeof(ctx, t)) match {
+          case TyVariant(fieldTys) =>
+            cases.foreach {
+              case (l, _, _) =>
+                fieldTys.find(_._1 == l) match {
+                  case Some(_) =>
+                  case None    => throw new Exception("label" + l + " is not in type")
+                }
+            }
+            val casetypes = cases map {
+              case (li, xi, ti) =>
+                val tyTi = fieldTys.find(_._1 == li) match {
+                  case Some(ty) => ty._2
+                  case None     => throw new Exception("label" + li + " is not found")
+                }
+                val ctx1 = ctx.addBinding(xi, VarBind(tyTi))
+                typeShift(-1, typeof(ctx1, ti))
+            }
+            casetypes.foldLeft(TyBot: Ty) { join(ctx, _, _) }
+          case TyBot => TyBot
+          case _     => throw new Exception("Expected variant type " + t)
+        }
+      case TmTag(li, ti, tyT) =>
+        simplifyTy(ctx, tyT) match {
+          case TyVariant(fieldTys) =>
+            fieldTys.find { _._1 == li } match {
+              case Some((_, tyTiExpected)) =>
+                val tyTi = typeof(ctx, ti)
+                if (subtype(ctx, tyTi, tyTiExpected))
+                  tyT
+                else
+                  throw new Exception("field doesn't have expected type in " + t)
+              case None => throw new Exception("label " + li + " not found " + t)
+            }
+          case z =>
+            println(z)
+            throw new Exception("annotation is not a variant type: " + t)
+        }
+      case TmLet(x, t1, t2) =>
+        val tyT1 = typeof(ctx, t1)
+        val ctx1 = ctx.addBinding(x, VarBind(tyT1))
+        typeShift(-1, typeof(ctx1, t2))
+      case TmUnit =>
+        TyUnit
+      case TmFix(t1) =>
+        val tyT1 = typeof(ctx, t1)
+        simplifyTy(ctx, tyT1) match {
+          case TyArr(tyT11, tyT12) =>
+            if (subtype(ctx, tyT12, tyT11))
+              tyT12
+            else
+              throw new Exception(
+                "result of body not compatible with domain " + t + " : " + tyT12 + " != " + tyT11
+              )
+          case _ =>
+            throw new Exception("arrow type expected in " + t1)
+        }
+    }
+
+  // ---------------------------------
+
   def subtype(ctx: Context, ty1: Ty, ty2: Ty): Boolean = {
     if (tyEqv(ctx, ty1, ty2)) {
       return true
@@ -279,144 +419,6 @@ object Typer {
           TyArr(join(ctx, tyS1, tyT1), meet(ctx, tyS2, tyT2))
         case _ => TyBot
       }
-    }
-
-  def typeof(ctx: Context, t: Term): Ty =
-    t match {
-      case TmVar(i, _) =>
-        ctx.getType(i)
-      case TmAbs(v, tyT1, t2) =>
-        val ctx1 = ctx.addBinding(v, VarBind(tyT1))
-        val tyT2 = typeof(ctx1, t2)
-        TyArr(tyT1, typeShift(-1, tyT2))
-      case TmApp(t1, t2) =>
-        val tyT1 = typeof(ctx, t1)
-        val tyT2 = typeof(ctx, t2)
-        simplifyTy(ctx, tyT1) match {
-          case TyArr(tyT11, tyT12) =>
-            if (subtype(ctx, tyT2, tyT11))
-              tyT12
-            else {
-              println(tyT1)
-              println(tyT2)
-              println(simplifyTy(ctx, tyT1))
-              throw new Exception("parameter mismatch in " + t + " : " + tyT2 + " != " + tyT11)
-            }
-          case TyBot => TyBot
-          case z => {
-            println(tyT1)
-            println(tyT2)
-            println(simplifyTy(ctx, tyT1))
-            throw new Exception("arrow type expected in " + t1)
-          }
-        }
-      case TmTrue =>
-        TyBool
-      case TmFalse =>
-        TyBool
-      case TmIf(t1, t2, t3) =>
-        if (subtype(ctx, typeof(ctx, t1), TyBool)) {
-          join(ctx, typeof(ctx, t2), typeof(ctx, t3))
-        } else {
-          throw new Exception("guard of conditional " + t + " is not a boolean")
-        }
-      case TmLet(x, t1, t2) =>
-        val tyT1 = typeof(ctx, t1)
-        val ctx1 = ctx.addBinding(x, VarBind(tyT1))
-        typeShift(-1, typeof(ctx1, t2))
-      case TmRecord(fields) =>
-        val fieldTys = fields.map { case (li, ti) => (li, typeof(ctx, ti)) }
-        TyRecord(fieldTys)
-      case TmProj(t1, l) =>
-        simplifyTy(ctx, typeof(ctx, t1)) match {
-          case TyRecord(fieldTys) =>
-            fieldTys find { _._1 == l } match {
-              case Some((_, tyi)) => tyi
-              case None           => throw new Exception("Label " + l + " not found in " + t)
-            }
-          case _ => throw new Exception("Expected record type for " + t1)
-        }
-      case TmCase(t, cases) =>
-        simplifyTy(ctx, typeof(ctx, t)) match {
-          case TyVariant(fieldTys) =>
-            cases.foreach {
-              case (l, _, _) =>
-                fieldTys.find(_._1 == l) match {
-                  case Some(_) =>
-                  case None    => throw new Exception("label" + l + " is not in type")
-                }
-            }
-            val casetypes = cases map {
-              case (li, xi, ti) =>
-                val tyTi = fieldTys.find(_._1 == li) match {
-                  case Some(ty) => ty._2
-                  case None     => throw new Exception("label" + li + " is not found")
-                }
-                val ctx1 = ctx.addBinding(xi, VarBind(tyTi))
-                typeShift(-1, typeof(ctx1, ti))
-            }
-            casetypes.foldLeft(TyBot: Ty) { join(ctx, _, _) }
-          case TyBot => TyBot
-          case _     => throw new Exception("Expected variant type " + t)
-        }
-      case TmFix(t1) =>
-        val tyT1 = typeof(ctx, t1)
-        simplifyTy(ctx, tyT1) match {
-          case TyArr(tyT11, tyT12) =>
-            if (subtype(ctx, tyT12, tyT11))
-              tyT12
-            else
-              throw new Exception(
-                "result of body not compatible with domain " + t + " : " + tyT12 + " != " + tyT11
-              )
-          case _ =>
-            throw new Exception("arrow type expected in " + t1)
-        }
-      case TmTag(li, ti, tyT) =>
-        simplifyTy(ctx, tyT) match {
-          case TyVariant(fieldTys) =>
-            fieldTys.find { _._1 == li } match {
-              case Some((_, tyTiExpected)) =>
-                val tyTi = typeof(ctx, ti)
-                if (subtype(ctx, tyTi, tyTiExpected))
-                  tyT
-                else
-                  throw new Exception("field doesn't have expected type in " + t)
-              case None => throw new Exception("label " + li + " not found " + t)
-            }
-          case z =>
-            println(z)
-            throw new Exception("annotation is not a variant type: " + t)
-        }
-      case TmAscribe(t1, tyT) =>
-        if (subtype(ctx, typeof(ctx, t1), tyT))
-          tyT
-        else
-          throw new Exception("body of as-term doesn't have the expected type in " + t)
-      case TmString(_) =>
-        TyString
-      case TmUnit =>
-        TyUnit
-      case TmZero =>
-        TyNat
-      case TmSucc(t1) =>
-        if (subtype(ctx, typeof(ctx, t1), TyNat)) {
-          TyNat
-        } else {
-          throw new Exception("argument of Succ: is not a number: " + t)
-        }
-      case TmPred(t1) =>
-        if (subtype(ctx, typeof(ctx, t1), TyNat)) {
-          TyNat
-        } else {
-          throw new Exception("argument of Pred: is not a number: " + t)
-        }
-      case TmIsZero(t1) =>
-        if (subtype(ctx, typeof(ctx, t1), TyNat)) {
-          TyBool
-        } else {
-          throw new Exception("argument of IsZero: is not a number: " + t)
-        }
     }
 }
 
